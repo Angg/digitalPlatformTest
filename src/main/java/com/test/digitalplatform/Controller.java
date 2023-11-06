cd7eafef825c23b4ec423e55398988e85974cda3
Angga Pratama Putra <adl.anggap@xl.co.id>
2023-11-06 19:53:46 +0700

    add comment

--- src/main/java/com/test/digitalplatform/Controller.java
+++ src/main/java/com/test/digitalplatform/Controller.java
@@ -17,17 +17,20 @@ public class Controller {
     RestTemplate restTemplate = new RestTemplate();
 
 
+    /* Get list of users from ReqRes */
     @GetMapping("/test1")
     public ResponseEntity test1() {
         return restTemplate.getForEntity("https://reqres.in/api/users?page=1", String.class);
     }
 
+    /* Post user data to ReqRes */
     @GetMapping("/test2")
     public ResponseEntity test2() {
         ObjectDAO reqPayload = new ObjectDAO().setName("morpheus").setJob("leader");
         return restTemplate.postForEntity("https://reqres.in/api/users", reqPayload, ObjectDAO.class);
     }
 
+    /* Get specific user with id number */
     @GetMapping("/test3/{id}")
     public ResponseEntity test3(@PathVariable String id) {
         return restTemplate.getForEntity("https://reqres.in/api/users/"+id, String.class);
